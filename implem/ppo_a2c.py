2b8011e7f546fbaed52b8ab4d70c25195913020c
Romain Blazevic <romain.blazevic@gmail.com>
2023-02-11 19:25:30 +0100

    save function

---
+++ implem/ppo_a2c.py
@@ -0,0 +1,9 @@
+from implem import logger
+
+
+# Use the logger to log some messages
+logger.debug("This is a debug log")
+logger.info("This is an info log")
+logger.warning("This is a warning log")
+logger.error("This is an error log")
+logger.critical("This is a critical log")
